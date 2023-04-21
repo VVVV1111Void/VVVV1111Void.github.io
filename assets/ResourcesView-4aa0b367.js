3a2672088406e7ebdbeecbbd0c0cc36d2ed79bfe
VVVV1111Void <lawrencecastillo424@gmail.com>
2023-04-21 09:10:59 +0800

    Bogus solution

--- assets/ResourcesView-4aa0b367.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as s,c as o,a as t}from"./index-d95672fe.js";const _={},c={class:"about"},n=t("h1",null,"WIP",-1),a=[n];function r(i,d){return s(),o("div",c,a)}const u=e(_,[["render",r]]);export{u as default};
